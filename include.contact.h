b95b07d002a5781afe46faf8acc9177706269b42
cheonchaewon <chaewonsky@naver.com>
2026-01-28 23:51:33 +0900

    Update include.contact.h

--- include.contact.h
+++ include.contact.h
@@ -1,9 +1,19 @@
 #ifndef CONTACT_H
 #define CONTACT_H
-  
-  typedef struct {
+
+#define MAX_CONTACTS 100
+
+typedef struct {
     char name[50];
-    char name[20];
-}Contact;
+    char phone[20];
+} Contact;
+
+typedef struct {
+    Contact list[MAX_CONTACTS];
+    int count;
+} AddressBook;
+
+void addContact(AddressBook *ab, const char *name, const char *phone);
+void listContacts(AddressBook *ab);
 
 #endif
